9ccdb87cb715602268361bf464ac8d5b5493c081
Penguin2 <vanessleonard.capuras@cit.edu>
2024-10-29 23:30:35 +0800

    Done Backend

---
+++ src/main/java/com/example/usersavorspace/services/UserService.java
@@ -0,0 +1,26 @@
+package com.example.usersavorspace.services;
+
+
+import com.example.usersavorspace.entities.User;
+import com.example.usersavorspace.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
---
+++ src/test/java/com/example/usersavorspace/UserSavorSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersavorspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSavorSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
